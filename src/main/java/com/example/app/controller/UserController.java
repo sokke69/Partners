ae1da721de131be7be8d81c7f9c4c305ca772103
sokke <sokke@example.com>
2022-09-30 15:25:48 +0900

    セッションにユーザーROLE記憶の命令文の修正。取得した文字の修正必要あり。

--- src/main/java/com/example/app/controller/UserController.java
+++ src/main/java/com/example/app/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.example.app.controller;
 
-import java.util.Collection;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -37,8 +34,7 @@ public class UserController {
 	public String topGet() throws Exception{
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		UserDetails userDetails = (UserDetails) auth.getPrincipal();
-		Collection<? extends GrantedAuthority> userRoles = auth.getAuthorities();
-		userRoles.toString();
+		String userRoles = auth.getAuthorities().toString();
 		String loginId = userDetails.getUsername();
 		System.out.println("login_id : " + loginId);
 		System.out.println("login_role : " + userRoles);
