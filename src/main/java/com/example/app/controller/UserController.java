79bc6da2c5c852b4af04fc3e1fae62f5edb35115
sokke <sokke@example.com>
2022-09-30 15:10:10 +0900

    /user/top/遷移時にセッションにログインID記憶。+軽微な修正。

--- src/main/java/com/example/app/controller/UserController.java
+++ src/main/java/com/example/app/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.app.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +21,9 @@ public class UserController {
 	@Autowired
 	UserService userService;
 	
+	@Autowired
+	HttpSession session;
+	
 	@GetMapping("/userList")
 	public String selectUserAll(Model model) throws Exception {
 		model.addAttribute("userList", userService.getUserAllDetailList());
@@ -27,14 +32,18 @@ public class UserController {
 	
 	@GetMapping("/top")
 	public String topGet() throws Exception{
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		UserDetails userDetail = (UserDetails) auth.getPrincipal();
+		String loginId = userDetail.getUsername();
+		
+		session.setAttribute("login_id", loginId);
+		
 		return "/user/top";
 	}
 	
 	@GetMapping("/mypage")
 	public String mypageGet(Model model) throws Exception{
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		UserDetails userDetail = (UserDetails) auth.getPrincipal();
-		String loginId = userDetail.getUsername();
+		String loginId = (String) session.getAttribute("login_id");
 		User user = new User();
 		user = userService.getUserByLoginId(loginId);
 		model.addAttribute("user", user);
